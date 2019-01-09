24fcf21bb2a62f401b696aae437f4401da2a07c4
mandersen21 <mikkelandersen21@gmail.com>
2019-01-09 14:12:27 +0100

    Added barcode example

--- appcore/src/main/java/org/appcore/BarcodeGenerator.java
+++ appcore/src/main/java/org/appcore/BarcodeGenerator.java
@@ -1,11 +1,95 @@
 package src.main.java.org.appcore;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.commons.lang3.StringUtils;
+import org.krysalis.barcode4j.impl.code39.Code39Bean;
+import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider;
+import org.krysalis.barcode4j.tools.UnitConv;
+
 public class BarcodeGenerator {
 
-	public BarcodeGenerator() {
-		
+	/**
+	 * Generate files
+	 *
+	 * @param msg
+	 * @param path
+	 * @return
+	 */
+	public static File generateFile(String msg, String path) {
+		File file = new File(path);
+		try {
+			generate(msg, new FileOutputStream(file));
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+		return file;
 	}
-	
-	
-	
+
+	/**
+	 * Generate bytes
+	 *
+	 * @param msg
+	 * @return
+	 */
+	public static byte[] generate(String msg) {
+		ByteArrayOutputStream ous = new ByteArrayOutputStream();
+		generate(msg, ous);
+		return ous.toByteArray();
+	}
+
+	/**
+	 * Generate to stream
+	 *
+	 * @param msg
+	 * @param ous
+	 */
+	public static void generate(String msg, OutputStream ous) {
+		if (StringUtils.isEmpty(msg) || ous == null) {
+			return;
+		}
+
+		Code39Bean bean = new Code39Bean();
+
+		// accuracy
+		final int dpi = 150;
+		// module width
+		final double moduleWidth = UnitConv.in2mm(1.0f / dpi);
+
+		// configuration object
+		bean.setModuleWidth(moduleWidth);
+		bean.setWideFactor(3);
+		bean.doQuietZone(false);
+
+		String format = "image/png";
+		try {
+
+			// output to the stream
+			BitmapCanvasProvider canvas = new BitmapCanvasProvider(ous, format, dpi, BufferedImage.TYPE_BYTE_BINARY,
+					false, 0);
+
+			// Generate a barcode
+			bean.generateBarcode(canvas, msg);
+
+			// end drawing
+			canvas.finish();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void main(String[] args) {
+		String msg = "123456789";
+		String path = "barcode.png";
+		System.out.print(msg);
+		System.out.print(path);
+		generateFile(msg, path);
+	}
+
 }
