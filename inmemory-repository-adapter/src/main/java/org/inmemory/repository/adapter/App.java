7c4e112813470db391311246b1865d9238ebaef4
mandersen21 <mikkelandersen21@gmail.com>
2019-01-09 13:02:00 +0100

    First commit

---
+++ inmemory-repository-adapter/src/main/java/org/inmemory/repository/adapter/App.java
@@ -0,0 +1,13 @@
+package org.inmemory.repository.adapter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
